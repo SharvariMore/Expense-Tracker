3b3824c96394f1f6fa2332aaea90e877b9fe5a44
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2024-06-07 14:01:06 -0400

    Set up express server on port and connect to db

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,30 @@
 
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./db/db');
+const { readdirSync } = require('fs');
+
+const app = express();
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(cors())
+
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
+
+const server = () => {
+        db()
+        app.listen(PORT, () => {
+        console.log("Listening on port: ", PORT);
+    })
+}
+
+server()
+
+console.log('PORT:', process.env.PORT);
