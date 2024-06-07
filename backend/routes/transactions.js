467b56748c95a49f5ed7c9f85de546c796aba379
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2024-06-07 14:05:31 -0400

    Added routes for transactions

---
+++ backend/routes/transactions.js
@@ -0,0 +1,13 @@
+const { addExpense, getExpenses, deleteExpenses } = require('../controllers/expense');
+const { addIncome, getIncomes, deleteIncomes } = require('../controllers/income');
+
+const router = require('express').Router();
+
+router.post('/add-income', addIncome)
+       .get('/get-incomes', getIncomes)
+       .delete('/delete-incomes/:id', deleteIncomes)
+       .post('/add-expense', addExpense)
+       .get('/get-expenses', getExpenses)
+       .delete('/delete-expenses/:id', deleteExpenses)
+
+module.exports = router
